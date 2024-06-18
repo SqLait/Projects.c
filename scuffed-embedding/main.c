b58403b5c6b8b9557858bf6443fa8bfa3b43033b
SqL <119504721+SqLait@users.noreply.github.com>
2024-06-18 11:20:06 +0200

    Added scuffed embedding
    This is just stupid but alr I guess

---
+++ scuffed-embedding/main.c
@@ -0,0 +1,15 @@
+/*This will not be the most beautiful embedding you have ever seen.
+ * This is because this excersize was done on windows. Not on linux
+ * where I could have done everything natively.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  char* input;
+  system("python3 script.py");
+
+  printf("\n\n This was my attempt of scuffed embedding... Hope you enjoyed!\n");
+  printf("This has no use case... just trying to see how I can run code from other langs inside C");
+  return 0;
+}
